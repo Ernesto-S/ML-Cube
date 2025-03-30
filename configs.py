a380c1438fa87cac20025ad5b8bb08db90a3a5dc
Ernesto-S <ernesto.salcedo.engr@gmail.com>
2025-03-30 11:17:38 -0700

    adding a git ignore and config for camera

---
+++ configs.py
@@ -0,0 +1,16 @@
+import raylibpy as rl #library for displaying windows
+
+windows_w = 600
+windows_h = 450
+fps = 60
+
+#3D Camera 
+camera = rl.Camera3D(
+    rl.Vector3(18.0, 16.0, 18.0), # Position
+    rl.Vector3(0.0, 0.0, 0.0), # Target camera center
+    rl.Vector3(0.0, 1.0, 0.0), # Define up orientation
+    45.0, # Field of view
+    rl.CAMERA_PERSPECTIVE # Camera Mode
+    )
+
+print(camera)
