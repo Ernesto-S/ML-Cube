c69fed00d05393a209afa13e47fa54c6d2979156
Ernesto-S <ernesto.salcedo.engr@gmail.com>
2025-03-30 19:29:20 -0700

    created a single piece to be displayed

--- rubik.py
+++ rubik.py
@@ -1,3 +1,42 @@
 import raylibpy as rl
 import numpy as np
 
+class Cube:
+    def __init__(self, size, position, face_color):
+        self.orientation = np.eye(3) # 3x3 identity matrix
+        self.size = size # size of the cube
+        self.position = position
+        self.face_color = face_color # color of the face
+
+        # Initialize empy list for models and face colors
+        self.model = None
+        self.gen_meshe(size)
+
+        # Create and position the meshes
+        self.create_model()
+
+    def gen_meshe(self, scale):
+        # Generate the mesh for the cube
+        self.mesh = rl.gen_mesh_cube(scale, scale, scale) # Generate a cube mesh
+
+    def create_model(self):
+        # Create the model for the cube
+        self.model = rl.load_model_from_mesh(self.mesh)
+        self.model.transform = rl.matrix_translate(self.position[0], self.position[1], self.position[2])
+
+    def __repr__(self):
+        return f"Cube(Model: {self.model}, Position: {self.position}, Face Color: {self.face_color})"
+
+class Rubik:
+    def __init__(self) -> None:
+        self.generate_rubik(1)
+        
+    def generate_rubik(self, size:int):
+        position = [0, 0, 0]
+        piece = Cube(size, position, rl.BLACK)
+        print("\n Cube Properties:")
+        print("Model:", piece.model)
+        print("Position:", piece.position)
+        print("Face Color:", piece.face_color)
+        print("\n")
+        return piece
--- world.py
+++ world.py
@@ -1,11 +1,14 @@
 import raylibpy as rl
 import configs
+from rubik import Rubik
 
 rl.init_window(
     configs.windows_w, # width of the window
     configs.windows_h, # height of the window
     title= "Virtual Rubiks Cube" 
 )
+rubik_cube = Rubik() # Create the rubik cube
+piece = rubik_cube.generate_rubik(size=3) # Generate the rubik cube
 
 rl.set_target_fps(configs.fps) # set the frames per second
 
@@ -17,8 +20,11 @@ while not rl.window_should_close():
     
     rl.begin_drawing() # Begin drawing
     rl.clear_background(rl.RAYWHITE) # Clear the background
-
     rl.begin_mode3d(configs.camera) # Begin 3D mode
+
+    position = rl.Vector3(piece.position[0], piece.position[1], piece.position[2]) # Set the position of the cube
+    
+    rl.draw_model(piece.model, position, 2, piece.face_color) # Draw the model
     rl.draw_grid(
         20, # Number of lines in the grid
         1.0 # Spacing between lines
